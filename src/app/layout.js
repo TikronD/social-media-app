d57d1155af33b9cb2b370b6b1862253123e4f2da
Tikron <d.hahn.gb@gmail.com>
2024-02-01 15:12:40 +0000

    added SQL

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,11 +2,10 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider, UserButton } from "@clerk/nextjs";
 
-Clerk;
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "social Media App",
+  title: "Social Media App",
   description:
     "This app has a user sign up with follow, like and messaging applications",
 };
@@ -14,11 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
-      {/* <div className="h-screen"> */}
-      <UserButton afterSignOutUrl="/" />
-      {/* </div> */}
       <html lang="en">
         <body className={inter.className}>
+          <div className="h-screen">
+            <UserButton afterSignOutUrl="/" />
+          </div>
           <h1>Movie Discussions</h1>
           {children}
         </body>
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,14 @@
-export default function Home() {
+import pg from "pg";
+
+// connection to database
+const dbConnectionString = process.env.DATABASE_URL;
+const db = new pg.Pool({
+  dbConnectionString: dbConnectionString,
+});
+
+export default async function Home() {
+  const results = await db.query("SELECT * FROM profiles");
+  console.log(results);
   return (
     <div>
       <h2>Home</h2>
