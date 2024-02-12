e66501345db05c16cd09b09f4ad9d41fd9a3cd3b
Tikron <d.hahn.gb@gmail.com>
2024-02-12 12:05:47 +0000

    removed radix theme Button

--- src/app/components/SubmitButton.js
+++ src/app/components/SubmitButton.js
@@ -1,13 +1,12 @@
 "use client";
-import { Button } from "@radix-ui/themes";
 import { useFormStatus } from "react-dom";
 
 export default function SubmitButton() {
   const { pending } = useFormStatus();
 
   return (
-    <Button disabled={pending} className={pending ? "disabled" : ""}>
+    <button disabled={pending} className={pending ? "disabled" : ""}>
       {pending ? "Creating Profile" : "Save Profile"}
-    </Button>
+    </button>
   );
 }
